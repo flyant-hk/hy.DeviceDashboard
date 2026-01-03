f75bc5563424b71322cf7b7ceeb193e3eb2b94d4
cedu <1903939182@qq.com>
2026-01-03 11:02:21 +0800

    添加项目文件。

---
+++ hy.DeviceDashboard/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace hy.DeviceDashboard
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
